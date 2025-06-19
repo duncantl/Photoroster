7d489f442ae262c9971703101a2ec985b85e90e0
Duncan Temple Lang <duncan@r-project.org>
2025-06-19 15:31:11 -0700

    use openxlsx:: prefix.  made width and height of image a parameter - dims and scale.

--- R/mkXLSX.R
+++ R/mkXLSX.R
@@ -1,5 +1,5 @@
 mkPhotoSheet =
-function(ros, out = "participation.xlsx", dir = "Images")
+function(ros, out = "participation.xlsx", dir = "Images", dims = c(272, 272), scale = 3)
 {
     r = ros
     d = r[, c("Student ID", "Last Name", "First Name")]
@@ -7,25 +7,24 @@ function(ros, out = "participation.xlsx", dir = "Images")
     file = file.path(dir, paste0(d$"Student ID", ".png"))
     w = file.exists(file)
 
-    library(openxlsx)
-    wb = createWorkbook("Participation Grade")
-    addWorksheet(wb, "Participation")
-    writeData(wb, 1, d)
+    #library(openxlsx)
+    wb = openxlsx::createWorkbook("Participation Grade")
+    openxlsx::addWorksheet(wb, "Participation")
+    openxlsx::writeData(wb, 1, d)
 
     rows = seq(1, nrow(d)) + 1L
-    wd = 272
-    ht = 272
-    fac = 3
+    dims = dims*scale
 
     z = invisible(mapply(function(img, idx)
-                        insertImage(wb, 1, img, startRow = idx, startCol = 4, width = wd*fac, height = ht*fac, units = "px"),
+                          openxlsx::insertImage(wb, 1, img, startRow = idx, startCol = 4,
+                                                width = dims[1], height = dims[2], units = "px"),
                   file[w],
                   rows[w]))
 
-    setRowHeights(wb, 1, rows, ht)
-    setColWidths(wb, 1, 1:3, c(20, 75, 20))
+    openxlsx::setRowHeights(wb, 1, rows, dims[1])
+    openxlsx::setColWidths(wb, 1, 1:3, c(20, 75, 20))
     if(length(out) && !is.na(out)) {
-        saveWorkbook(wb, out, overwrite = TRUE)
+        openxlsx::saveWorkbook(wb, out, overwrite = TRUE)
         out
     } else
         wb
